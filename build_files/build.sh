514d83f0b9b6087c883284316621eda37dd4adbd
oparada1988 <oparada1988@gmail.com>
2025-04-21 14:51:15 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -7,6 +7,7 @@ dnf5 remove gnome-shell-extension-hanabi
 dnf5 remove gnome-shell-extension-caffeine
 dnf5 remove gnome-shell-extension-compiz-windows-effect
 dnf5 remove gnome-shell-extension-blur-my-shell
+dnf5 remove gnome-shell-extension-gsconnect
 dnf5 remove sunshine
 dnf5 remove waydroid
 dnf5 remove input-remapper
