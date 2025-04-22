06ed16a3dc6ab332adf8b61ad2fa06e72dcd6cac
oparada1988 <oparada1988@gmail.com>
2025-04-22 15:52:45 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,18 +3,18 @@
 set -ouex pipefail
 
 # remove unwanted packages
-dnf5 remove gnome-shell-extension-hanabi
-dnf5 remove gnome-shell-extension-caffeine
-dnf5 remove gnome-shell-extension-compiz-windows-effect
-dnf5 remove gnome-shell-extension-blur-my-shell
-dnf5 remove gnome-shell-extension-gsconnect
-dnf5 remove sunshine
-dnf5 remove waydroid
-dnf5 remove input-remapper
-dnf5 remove rom-properties
-dnf5 remove discover-overlay
-dnf5 remove btrfs-assistant
-dnf5 remove firewalld
+dnf5 remove gnome-shell-extension-hanabi \
+      gnome-shell-extension-caffeine \
+      gnome-shell-extension-compiz-windows-effect \
+      gnome-shell-extension-blur-my-shell \
+      gnome-shell-extension-gsconnect \
+      sunshine \
+      waydroid \
+      input-remapper \
+      rom-properties \
+      discover-overlay \
+      btrfs-assistant \
+      firewalld \
 
 # remove unwanted flatpaks
 flatpak --assumeyes uninstall io.github.flattool.Warehouse \
