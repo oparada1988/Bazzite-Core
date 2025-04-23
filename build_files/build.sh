1b83e9365aeaae7be4ee57163b7bf505b44548c4
oparada1988 <oparada1988@gmail.com>
2025-04-23 15:53:33 -0400

    Update build.sh

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 
 # Function to configure a custom wallpaper
 configure_wallpaper() {
-    local wallpaper_path=/Wallpaper 1.png
+    local wallpaper_path="./Wallpaper 1.png"
     if [[ -f "$wallpaper_path" ]]; then
         echo "Setting custom wallpaper from $wallpaper_path"
         mkdir -p /usr/share/backgrounds/custom/
@@ -43,40 +43,3 @@ flatpak --assumeyes uninstall io.github.flattool.Warehouse \
 # install flatpak packages
 flatpak --assumeyes install com.google.Chrome \
       com.discordapp.Discord \
-
-# Prompt for custom wallpaper
-read -p "Enter the full path to your custom wallpaper (or leave empty to skip): " wallpaper_path
-if [[ -n "$wallpaper_path" ]]; then
-    configure_wallpaper "$wallpaper_path"
-else
-    echo "Skipping custom wallpaper configuration."
-fi
-# remove unwanted packages
-dnf5 remove gnome-shell-extension-hanabi \
-      gnome-shell-extension-caffeine \
-      gnome-shell-extension-compiz-windows-effect \
-      gnome-shell-extension-blur-my-shell \
-      gnome-shell-extension-gsconnect \
-      sunshine \
-      waydroid \
-      input-remapper \
-      rom-properties \
-      discover-overlay \
-      btrfs-assistant \
-      firewalld \
-# install packages
-dnf5 install gnome-shell-extension-dash-to-dock
-
-# remove unwanted flatpaks
-flatpak --assumeyes uninstall io.github.flattool.Warehouse \
-      io.github.dvlv.boxbuddyrs  \
-      com.vysp3r.ProtonPlus \
-      com.github.Matoking.protontricks \
-
-
-# install flatpak packages
-flatpak --assumeyes install com.google.Chrome \
-      com.discordapp.Discord \
-
-
-
